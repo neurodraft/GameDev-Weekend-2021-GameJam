fc8c8d180f5b216b020041120878c89beb062d3b
Cybeertt <rafaelbpalma@gmail.com>
2021-04-24 19:53:47 +0100

    Add slow

--- Assets/Scripts/Domino.cs
+++ Assets/Scripts/Domino.cs
@@ -8,13 +8,13 @@ public class Domino : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+		
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {                 
+
     }
 
     void OnCollisionEnter(Collision collision){
